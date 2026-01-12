2b03d177bfa7daf9be3b5aad05a516ec8b3c3cb2
OmPatel9019 <op24942494@gmail.com>
2026-01-12 22:22:35 +0530

    refactor: update dialog and tooltip UI components

--- app/dashboard/component/sidebar/index.tsx
+++ app/dashboard/component/sidebar/index.tsx
@@ -1,7 +1,10 @@
+
+import { NewButton } from "./new-button";
+
 export const Sidebar = () => {
     return (
         <aside className="fixed z-[1] left-0 bg-green-500 h-full w-[60px] flex-col flex px-3 gap-y-4 text-white text-bold">
-            +
+            <NewButton/>
         </aside>
     );
 };
--- app/dashboard/component/sidebar/navbar.tsx
+++ app/dashboard/component/sidebar/navbar.tsx
@@ -5,6 +5,9 @@ import { UserButton } from "@clerk/nextjs";
 export const Navbar = () => {
     return (
         <div className="flex items-center p-[18px] gap-x-4 bg-blue-400">
+            <div className="flex w-full p-[4px] bg-white rounded-[18px]">
+               <h2>search</h2>
+            </div>
             <UserButton/>
         </div>
     );
