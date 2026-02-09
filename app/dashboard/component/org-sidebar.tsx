ff923fd47280c8c15b70ea09213b0aaf8765edb4
OmPatel9019 <op24942494@gmail.com>
2026-02-09 16:23:43 +0530

    Add a organization switcher

--- app/dashboard/component/org-sidebar.tsx
+++ app/dashboard/component/org-sidebar.tsx
@@ -1,10 +1,38 @@
 // we "use client" as it is client side
 "use client"
 
+import Link from "next/link";
+import Image from "next/image";
+import {Poppins} from "next/font/google";
+import {cn} from "@/lib/utils";
+import { OrganizationSwitcher } from "@clerk/nextjs";
+
+const fonts = Poppins({
+    subsets: ["latin"],
+    weight: ["600"],
+});
+    
 export const Orgsidebar = () => {
     return (
-        <div className="hidden lg:flex flex-col h-full w-[206px] space-y-4 pl-[5px] pt-[5px] bg-cyan-600">
-            Orgsidebar
+        <div className="hidden lg:flex flex-col h-full w-[204px] space-y-4 pl-[5px] pt-[5px] ">
+            <Link href="/">
+                <div className="flex items-center gap-x-4">
+                    <Image
+                        src="/logo.png"
+                        alt="Logo"
+                        width={60}
+                        height={60}
+                    />
+                    <span className={cn("text-2xl text-black-600", fonts.className)}>
+                        Sketchly
+                    </span>
+                </div>
+            </Link>
+            <div className="text-2xl flex-col items-center w-[204px] px-8 py-0 bg-green-500 ">
+               
+                    <OrganizationSwitcher />
+                
+            </div>
         </div>
     );
 };
