9e22d52b5ea184a2f234a1c0408d315a8bbc8ca1
OmPatel9019 <op24942494@gmail.com>
2026-04-21 11:05:46 +0530

    resolve ts error

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -154,7 +154,7 @@ export const Canvas = ({ boardId }: CanvasProps) => {
         })
         const ids = findInterSectingLayers(
             [...layerIds],
-            layers,
+            new Map(Object.entries(layers)),
             origin,
             current,
         );
